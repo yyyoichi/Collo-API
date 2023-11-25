399a3ada8d8987f64424c92434becceea5f82ba5
yyyoichi <origamiichiro@gmail.com>
2023-11-25 12:50:18 +0000

    feat add test

--- internal/pair/pair_test.go
+++ internal/pair/pair_test.go
@@ -1,6 +1,7 @@
 package pair
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -16,6 +17,17 @@ func TestT(t *testing.T) {
 		gots := r.getNouns()
 		require.Equal(t, len(expected), len(gots))
 		require.Equal(t, expected, gots)
+	})
+	t.Run("FetchMorpheme", func(t *testing.T) {
+		speech, err := NewSpeech(tconfig)
+		require.NoError(t, err)
+		for url := range speech.generateURL(context.Background()) {
+			fr := speech.fetch(url)
+			for _, s := range fr.getSpeechs() {
+				r := ma.parse(s)
+				require.NoError(t, r.err)
+			}
+		}
 	})
 	t.Run("PairChunk", func(t *testing.T) {
 		ps, err := NewPairStore(tconfig, thandler)
