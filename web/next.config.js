1b76cdb848d47d9b87c116caa9580d969f660d56
yyyoichi <origamiichiro@gmail.com>
2023-12-08 01:41:18 +0000

    fix warn

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  output: 'export',
-};
-
-module.exports = nextConfig;
+module.exports = {};
