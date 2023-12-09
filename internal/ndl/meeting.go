ac5d880508d39eb00f675a5ca1cd8bc30f72f280
yyyoichi <origamiichiro@gmail.com>
2023-12-09 05:20:03 +0000

    feat use meeting-api io

---
+++ internal/ndl/meeting.go
@@ -0,0 +1,162 @@
+package ndl
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
+	"time"
+	"yyyoichi/Collo-API/pkg/stream"
+)
+
+const meetingListUrlf = "https://kokkai.ndl.go.jp/api/meeting_list" // 会議単位簡易出力API
+const meetingUrlf = "https://kokkai.ndl.go.jp/api/meeting"          //会議単位出力API
+const maximumRecords = 10                                           // 一回の最大取得件数
+
+type FetchError struct{ error }
+
+type Meeting struct {
+	config          Config
+	numberOfRecords *int
+}
+
+func NewMeeting(config Config) *Meeting {
+	m := &Meeting{config: config}
+	m.init()
+	return m
+}
+
+func (m *Meeting) GenerateMeeting(ctx context.Context) <-chan *MeetingResult {
+	urls := m.getURLs()
+	if len(urls) == 0 {
+		mr := &MeetingResult{
+			err: FetchError{errors.New("not found data")},
+		}
+		return stream.Generator[*MeetingResult](ctx, mr)
+	}
+	urlCh := stream.Generator[string](ctx, urls...)
+	return stream.Line[string, *MeetingResult](ctx, urlCh, m.fetch)
+}
+
+func (m *Meeting) fetch(url string) *MeetingResult {
+	body, err := m.config.Fetcher(url)
+	if err != nil {
+		return &MeetingResult{err: FetchError{err}, url: url}
+	}
+	result := &MeetingResult{url: url}
+	if err := json.Unmarshal(body, &result.Result); err != nil {
+		return &MeetingResult{err: FetchError{err}, url: url}
+	}
+	// regular
+	if result.Result.Message == "" {
+		return result
+	}
+	// has error
+	return &MeetingResult{err: FetchError{errors.New(result.Result.Message)}, url: url}
+}
+
+func (m *Meeting) getURLs() []string {
+	numberOfRecords := m.getNumberOfRecords()
+	urls := []string{}
+	for i := 1; i <= numberOfRecords; i += maximumRecords {
+		urls = append(urls, m.createURL(i, maximumRecords, meetingUrlf))
+	}
+	return urls
+}
+
+func (m *Meeting) getInitURL() string {
+	return m.createURL(1, 1, meetingListUrlf)
+}
+
+func (m *Meeting) getNumberOfRecords() int {
+	if m.numberOfRecords == nil {
+		mr := m.fetch(m.getInitURL())
+		if mr.err != nil {
+			return 0
+		}
+		m.numberOfRecords = &mr.Result.NumberOfRecords
+	}
+	return *m.numberOfRecords
+}
+
+func (m *Meeting) createURL(start, max int, urlf string) string {
+	if max == 0 {
+		max = maximumRecords
+	}
+	params := url.Values{}
+	params.Add("maximumRecords", strconv.Itoa(max))
+	params.Add("any", m.config.Search.Any)
+	params.Add("from", m.config.Search.From.Format("2006-01-02"))
+	params.Add("until", m.config.Search.Until.Format("2006-01-02"))
+	params.Add("startRecord", strconv.Itoa(start))
+	params.Add("recordPacking", "json")
+	url := fmt.Sprintf("%s?%s", urlf, params.Encode())
+	return url
+}
+
+func (m *Meeting) init() {
+	if m.config.Fetcher == nil {
+		m.config.Fetcher = func(url string) (body []byte, err error) {
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				return nil, err
+			}
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				return nil, err
+			}
+
+			defer resp.Body.Close()
+			body, err = io.ReadAll(resp.Body)
+			return body, err
+		}
+	}
+	if m.config.Search.Any == "" {
+		m.config.Search.Any = "科学技術"
+	}
+	l, _ := time.LoadLocation("Asia/Tokyo")
+	if m.config.Search.From.IsZero() {
+		m.config.Search.From = time.Date(2023, 12, 1, 0, 0, 0, 0, l)
+	}
+	if m.config.Search.Until.IsZero() {
+		m.config.Search.Until = time.Date(2023, 12, 5, 0, 0, 0, 0, l)
+	}
+}
+
+type MeetingResult struct {
+	url    string
+	err    error
+	Result struct {
+		Message         string `json:"message"`
+		NumberOfRecords int    `json:"numberOfRecords"`
+		MeetingRecord   []struct {
+			SpeechRecord []struct {
+				Speech string `json:"speech"`
+			} `json:"speechRecord"`
+		} `json:"meetingRecord"`
+	}
+}
+
+var re, _ = regexp.Compile(`^○.*?　`)
+
+func (mr *MeetingResult) Error() error { return mr.err }
+func (mr *MeetingResult) URL() string  { return mr.url }
+
+// 会議ごとの発言をひとまとめにして返す。
+func (mr *MeetingResult) GetSpeechsPerMeeting() []string {
+	speechs := make([]string, len(mr.Result.MeetingRecord))
+	for i, meeting := range mr.Result.MeetingRecord {
+		for _, speech := range meeting.SpeechRecord {
+			s := speech.Speech
+			speechs[i] += re.ReplaceAllLiteralString(s, "")
+		}
+	}
+	return speechs
+}
