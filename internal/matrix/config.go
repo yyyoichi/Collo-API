0ccebf61de3ac8b549f061a81d6276f9f1dfb39f
yyyoichi <origamiichiro@gmail.com>
2023-12-13 04:28:59 +0000

    feat add config

--- internal/matrix/config.go
+++ internal/matrix/config.go
@@ -4,10 +4,15 @@ type WordRatingAlgorithm uint
 
 const VectorCentrality WordRatingAlgorithm = 1
 
+type CoOccurrencetNormalization uint
+
+const Dice CoOccurrencetNormalization = 1
+
 type Config struct {
-	Threshold           float64             // しきい値。上位Threshold%の値を使用する
-	MinWords            int                 // 最小単語数
-	MinScale            float64             // 重要度のスケーリング最小値
-	MaxScale            float64             // 重要度のスケーリング最大値
-	WordRatingAlgorithm WordRatingAlgorithm // 重要語判定アルゴリズム
+	ReduceThreshold            float64                    // しきい値。上位Threshold%の単語を使用する
+	MinNodes                   int                        // 最小単語数
+	MinNodeImportanceScale     float64                    // ノード重要度のスケーリング最小値
+	MaxNodeImportanceScale     float64                    // ノード重要度のスケーリング最大値
+	NodeRatingAlgorithm        WordRatingAlgorithm        // 重要語判定アルゴリズム
+	CoOccurrencetNormalization CoOccurrencetNormalization // 共起行列の正規化アルゴリズム
 }
